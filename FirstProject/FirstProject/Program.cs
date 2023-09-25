003a16db0325c8fad76f8c3b340f8bb6f16ef920
Brian Andersen <brianmanden@gmail.com>
2023-09-25 12:50:12 +0200

    ADD First Project

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Spectre;
+using Spectre.Console;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 19;
+            Console.WriteLine(x);
+
+            string name = "* Brian *";
+            Console.WriteLine(name);
+
+            string y = "12";
+            x = 14;
+
+            Console.WriteLine(x+y);
+            Console.WriteLine(int.Parse(y) + x);
+
+            AnsiConsole.Write(
+                new FigletText("Output:")
+                        .LeftJustified()
+                        .Color(Color.Green)
+                );
+            AnsiConsole.MarkupLine("[red]Name: [/]" + name);
+
+            Console.ReadKey();
+        }
+    }
+}
