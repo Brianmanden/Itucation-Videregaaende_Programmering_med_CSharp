053052717871ce04b190f91d9803e73fd5593c54
Brian Andersen <brianmanden@gmail.com>
2023-09-26 13:35:46 +0200

    ADD Exercise 1

---
+++ Dag 2/Second Project/ObjectAndClass/Models/Counter.cs
@@ -0,0 +1,35 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+using System;
+
+namespace ObjectAndClass.Models
+{
+    //--- Exercise 1 ---
+    //Class: Counter
+    //A simple tally counter that increments its value by 1 every click.
+    //Create the class with the following functionality:
+    //- 'Empty' constructor(no positional arguments required)
+    //- 'value' variable
+    //- 'Click()' method
+    //- 'Reset()' method
+    //Decide how to display your current value: After each function call? Only when asked?
+    //Create an object and test your class!
+
+    public class Counter
+    {
+        private int value = 0;
+
+        public Counter(){}
+
+        public int Click()
+        {
+            value++;
+            return value;
+        }
+
+        public void Reset()
+        {
+            value = 0;
+        }
+    }
+}
---
+++ Dag 2/Second Project/ObjectAndClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using ObjectAndClass.Models;
+
+Counter counter = new Counter();
+
+// Click tally counter a few times
+counter.Click();
+counter.Click();
+int currentVal = counter.Click();
+
+Console.WriteLine($"Current tally counter value is now: {currentVal}");
+
+// Reset the counter
+counter.Reset();
+
+// Click another time
+currentVal = counter.Click();
+Console.WriteLine($"Current tally counter value is now: {currentVal}");
