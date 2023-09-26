71681ace62c98457c61f45b15231ad56a834a25c
Brian Andersen <brianmanden@gmail.com>
2023-09-26 13:46:41 +0200

    ADD Exercise 2

---
+++ Dag 2/Second Project/ObjectAndClass/Models/Die.cs
@@ -0,0 +1,27 @@
+﻿namespace ObjectAndClass.Models
+{
+    //--- Exercise 2 ---
+    //Class: Die
+    //A 6-sided die that can be rolled!
+    //- Consider how/if to keep track of your 6 sides.
+    //- Implement a 'Roll()' method, which returns a random integer(1 - 6)
+    //hint: The Random class is very helpful
+
+    public class Die
+    {
+        private IList<int> _rolls = new List<int>();
+
+        public int Roll()
+        {
+            Random rnd = new Random();
+            int theRoll = rnd.Next(1, 7);
+            _rolls.Add(theRoll);
+            return theRoll;
+        }
+
+        public IList<int> RollHistory()
+        {
+            return _rolls;
+        }
+    }
+}
--- Dag 2/Second Project/ObjectAndClass/Program.cs
+++ Dag 2/Second Project/ObjectAndClass/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ObjectAndClass.Models;
 
+#region Exercise 1
+
 Counter counter = new Counter();
 
 // Click tally counter a few times
@@ -15,3 +17,42 @@ counter.Reset();
 // Click another time
 currentVal = counter.Click();
 Console.WriteLine($"Current tally counter value is now: {currentVal}");
+Console.WriteLine(Environment.NewLine);
+
+#endregion
+
+#region Exercise 2
+
+// Instantiate new die
+Die die = new Die();
+
+// Roll die multiple times
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+die.Roll();
+
+// Loop through roll history and print each roll
+foreach (var roll in die.RollHistory())
+{
+    Console.WriteLine(roll);
+}
+
+Console.WriteLine(Environment.NewLine);
+
+#endregion
+
+Console.WriteLine();
