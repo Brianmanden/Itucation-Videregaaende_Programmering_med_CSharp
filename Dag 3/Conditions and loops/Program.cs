67de30845902184d0e5569fadde465e2f1f9e9ad
Brian Andersen <brianmanden@gmail.com>
2023-09-27 14:02:49 +0200

    INIT solution ADD Exercise 1

---
+++ Dag 3/Conditions and loops/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Conditions_and_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---Exercise 1---
+            //Use a for loop to print each odd number from 1 to 20.
+            #region Exercise 1
+            for (int i = 0; i < 20; i++) { 
+                if (i % 2 == 1) {
+                    Console.WriteLine($"{i} is an uneven number.");
+                }
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
