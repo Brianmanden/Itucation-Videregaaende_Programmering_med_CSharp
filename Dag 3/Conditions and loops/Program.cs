74e9546ff47e517fbef51b4177ab411532d98c72
Brian Andersen <brianmanden@gmail.com>
2023-09-27 14:39:11 +0200

    ADD Exercise 3

--- Dag 3/Conditions and loops/Program.cs
+++ Dag 3/Conditions and loops/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Conditions_and_loops
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System;
+
+namespace Conditions_and_loops
 {
     internal class Program
     {
@@ -45,6 +49,44 @@
             {
                 Console.WriteLine("Too bad. No more tries");
             }
+
+            Console.WriteLine(Environment.NewLine);
+            #endregion
+
+            //---Exercise 3---
+            //A movie theater charges different ticket prices depending on a peron's age.
+            //If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10;
+            //and if they are over age 12, the ticket is $15.Write code in which you ask the users for an age, and then
+            //tell them the cost of a movie ticket.
+            #region Exercise 3
+
+            Console.WriteLine("Movie Theatre");
+            Console.WriteLine("Price ranges:");
+            Console.WriteLine("Age under 3 - price is free");
+            Console.WriteLine("Age between 3 and up to and including 12 - price is 10$");
+            Console.WriteLine("Age over 12 - price is 15$");
+
+            int age = 0;
+            int tries = 3;
+            do {
+                Console.WriteLine("What is the age of the person going to the movie theatre ?");
+                age = int.Parse(Console.ReadLine());
+
+                if (age < 3)
+                {
+                    Console.WriteLine("Ticket is free of charge.");
+                }
+                else if (age >= 3 && age <= 12)
+                {
+                    Console.WriteLine("Ticket price is: 10$");
+                }
+                else { 
+                    Console.WriteLine("Ticket price is: 15$");
+                }
+
+                tries--;
+            }
+            while (tries > 0);
 
             Console.WriteLine(Environment.NewLine);
             #endregion
