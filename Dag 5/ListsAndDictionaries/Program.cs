bdc5b75a58cb66fae9e5e685016113d7cc3c51af
Brian Andersen <brianmanden@gmail.com>
2023-09-30 08:54:21 +0200

    ADD Exercise1 a,b

--- Dag 5/ListsAndDictionaries/Program.cs
+++ Dag 5/ListsAndDictionaries/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //---Exercise 1: Lists---
+
+            //a.Store the names of a few of your friends in a list.
+            //Print each person's name one at a time.
+            List<string> friendsList = new List<string>
+            {
+                "Albert",
+                "Niels",
+                "John",
+                "Richard",
+                "Paul",
+                "Joe"
+            };
+
+            foreach (string friend in friendsList)
+            {
+                Console.WriteLine($"'Friend': {friend}");
+            }
+
+
+            Console.WriteLine(Environment.NewLine);
+
+
+            //b.Think of a hobby of yours, and make a list that stores several examples of items from that hobby.
+            //Use your list to print a series of statements about these items, such as "Squirtle is a pretty cool dude."
+            List<string> dotnetHobbyItems = new List<string>
+            {
+                "C#",
+                "Datatypes",
+                "Arrays",
+                "Lists",
+                "Collections"
+            };
+
+            Console.WriteLine($"{dotnetHobbyItems[0]} is a programming language in the .Net Framework.");
+            Console.WriteLine($"It contains a lot of {dotnetHobbyItems[1]}.");
+            Console.WriteLine($"Some examples are: {dotnetHobbyItems[2]}, {dotnetHobbyItems[3]}, {dotnetHobbyItems[4]}.");
+
+            Console.ReadKey();
         }
     }
 }
