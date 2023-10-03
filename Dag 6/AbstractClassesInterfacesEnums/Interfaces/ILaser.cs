f4d13bc7e515cc2c99bed6e5b3507025842d9cfb
Brian Andersen <brianmanden@gmail.com>
2023-10-03 12:26:39 +0200

    ADD Exercise 2

---
+++ Dag 6/AbstractClassesInterfacesEnums/Interfaces/ILaser.cs
@@ -0,0 +1,7 @@
+﻿namespace AbstractClassesInterfacesEnums.Interfaces
+{
+    internal interface ILaser
+    {
+        public void FireLaser();
+    }
+}
---
+++ Dag 6/AbstractClassesInterfacesEnums/Models/DeathStar.cs
@@ -0,0 +1,12 @@
+﻿using AbstractClassesInterfacesEnums.Interfaces;
+
+namespace AbstractClassesInterfacesEnums.Models
+{
+    internal class DeathStar : ILaser
+    {
+        public void FireLaser()
+        {
+            Console.WriteLine("Pew pew - lasers fired. An Obi Wan felt a great disturbance in the force, as if millions of voices suddenly cried out in terror.");
+        }
+    }
+}
