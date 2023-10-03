9551334f234725028cd3dcd98210192fa6728e02
Brian Andersen <brianmanden@gmail.com>
2023-10-03 10:29:22 +0200

    CLEAN UP

--- Dag 6/Inheritance/Models/Band.cs
+++ Dag 6/Inheritance/Models/Band.cs
@@ -3,6 +3,7 @@
     internal class Band : Artist
     {
         public string BestSong { get; set; }
+
         public override string PlaySong()
         {
             return $"The best song of {Name} is clearly {BestSong} of the genre: {Genre}";
