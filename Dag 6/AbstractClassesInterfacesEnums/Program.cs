90c1e70c45fa9d91b36dbdcb291e6f57b2bb76b6
Brian Andersen <brianmanden@gmail.com>
2023-10-03 12:59:13 +0200

    CLEAN UP ADD Comments

--- Dag 6/AbstractClassesInterfacesEnums/Program.cs
+++ Dag 6/AbstractClassesInterfacesEnums/Program.cs
@@ -1,5 +1,8 @@
 ﻿using AbstractClassesInterfacesEnums.Enums;
 using AbstractClassesInterfacesEnums.Models;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.Intrinsics.X86;
+using System;
 
 namespace AbstractClassesInterfacesEnums
 {
@@ -35,7 +38,13 @@ namespace AbstractClassesInterfacesEnums
 
             #region Exercise 2
 
+            //---Exercise 2: Interface---
+
+            //b.Create a class called DeathStar that implements the ILaser interface.
+            //You decide what the FireLaser method does.If you can't think of anything, just make it write/return "Pew pew".
             DeathStar deathStar = new DeathStar();
+
+            //c.Fire your lasers!
             deathStar.FireLaser();
 
             #endregion
@@ -43,13 +52,15 @@ namespace AbstractClassesInterfacesEnums
             Console.WriteLine(Environment.NewLine);
 
             #region Exercise 3
-            Random random = new Random();
+            //---Exercise 3: Enum---
 
-            //for (int i = 1; i <= 100; i++)
-            //{
-            //    Console.WriteLine($"[{i}] - Random card drawn: { (Face)random.NextInt64(13)} of { (Suit)random.NextInt64(4)}");
-            //}
-            
+            //b.Use an object of the Random class to select two suitable random numbers(use the Next method) :
+            //- Use one random number to get the face value of a card.
+            //- Use the other random number to get the suit value of a card.
+
+            //c.Print the card (face and suit)!
+
+            Random random = new Random();
             Console.WriteLine($"Random card drawn: { (Face)random.NextInt64(13)} of { (Suit)random.NextInt64(4)}");
 
             #endregion
