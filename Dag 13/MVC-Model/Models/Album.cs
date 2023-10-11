967687ce0917908144b4cac30dfc1d25f72f0590
Brian Andersen <brianmanden@gmail.com>
2023-10-11 11:35:06 +0200

    INIT Solution ADD Gitignore ADD Model Album WIP

---
+++ Dag 13/MVC-Model/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Model.Models
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        [CheckValidYear]
+        public int ReleaseYear { get; set; }
+    }
+}
