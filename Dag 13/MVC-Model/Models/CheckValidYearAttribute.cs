126e917047ba50b8550693994d17bcfad8632172
Brian Andersen <brianmanden@gmail.com>
2023-10-11 12:37:54 +0200

    UPDATE NuGet Dependencies

---
+++ Dag 13/MVC-Model/Models/CheckValidYearAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Model.Models
+{
+    internal class CheckValidYear : ValidationAttribute
+    {
+        public override bool IsValid(object value) {
+            int year = (int)value;
+            
+            if (year < 1960)
+            {
+                ErrorMessage = "No good albums released before 1960";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
