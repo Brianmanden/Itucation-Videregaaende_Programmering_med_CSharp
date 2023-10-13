8193fa2557c288f46cc9b9264df060c2494dedc7
Brian Andersen <brianmanden@gmail.com>
2023-10-13 15:41:29 +0200

    ADD Demo database 'VoresDB' to solution - code first

---
+++ Dag 15/Exercise2_MVC_DatabaseFirst/Models/Customer.cs
@@ -0,0 +1,32 @@
+namespace Exercise2_MVC_DatabaseFirst.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Customer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Customer()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        [Key]
+        public int cID { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string cName { get; set; }
+
+        [StringLength(45)]
+        public string cEmail { get; set; }
+
+        public int? cPhone { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Dag 15/Exercise2_MVC_DatabaseFirst/Models/Product.cs
@@ -0,0 +1,29 @@
+namespace Exercise2_MVC_DatabaseFirst.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Product()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        [Key]
+        public int pID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string pName { get; set; }
+
+        public int? pPrice { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
