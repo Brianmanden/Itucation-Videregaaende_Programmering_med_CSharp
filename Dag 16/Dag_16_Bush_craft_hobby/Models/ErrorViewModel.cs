be9e7b3efceb078bcf12483b53f3dd21b85b1540
Brian Andersen <brianmanden@gmail.com>
2023-10-16 14:05:21 +0200

    CLEAN UP

--- Dag 16/Dag_16_Bush_craft_hobby/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Dag_16_Bush_craft_hobby.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
