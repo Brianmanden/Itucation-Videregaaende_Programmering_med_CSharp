417cd826570d37f8db872ff5d4ff44f2acb3977b
Brian Andersen <brianmanden@gmail.com>
2023-10-16 13:30:09 +0200

    ADD Solution ADD Gitignore ADD Exercise 1 markdown file

---
+++ Dag 16/Dag_16_Bush_craft_hobby/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dag_16_Bush_craft_hobby.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
