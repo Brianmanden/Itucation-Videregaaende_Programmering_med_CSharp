1dd04daf7222155f9553bcad4b8c3a6563a698b1
Brian Andersen <brianmanden@gmail.com>
2023-10-20 12:34:37 +0200

    ADD Scaffolding to existing database KundeDB

---
+++ Dag 20/BlazorAppBlazorServerDemo/Data/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorAppBlazorServerDemo.Data;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public int Phone { get; set; }
+}
