5691cd31c544be08e03ed265f1e324e741d118a2
Brian Andersen <brianmanden@gmail.com>
2023-10-21 22:22:52 +0200

    ADD Scaffold DB from Azure DB using secrets

--- Eksamensprojekt/Eksamensprojekt/Server/Program.cs
+++ Eksamensprojekt/Eksamensprojekt/Server/Program.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Eksamensprojekt.Server.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<EksamensprojektContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:Eksamensprojekt"]));
 
 var app = builder.Build();
 
