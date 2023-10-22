46e97dcaaa0436c2b6902caf5cf3ce931f355794
Brian Andersen <brianmanden@gmail.com>
2023-10-22 14:22:59 +0200

    ADD Documents List View WIP

---
+++ Eksamensprojekt/BushcraftBlog/Shared/Models/Document.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BushcraftBlog.Shared.Models;
+
+public class Document
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Heading { get; set; }
+
+    public string Content { get; set; }
+}
