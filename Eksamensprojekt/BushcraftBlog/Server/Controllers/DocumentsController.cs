5ebbd948e1aaf6a8b957f0607dbb7c311b294909
Brian Andersen <brianmanden@gmail.com>
2023-10-22 20:10:35 +0200

    CLEAN UP Model and REFACTOR ADD Single Document View

--- Eksamensprojekt/BushcraftBlog/Server/Controllers/DocumentsController.cs
+++ Eksamensprojekt/BushcraftBlog/Server/Controllers/DocumentsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BushcraftBlog.Server.Models;
+using BushcraftBlog.Shared.Models;
 
 namespace BushcraftBlog.Server.Controllers
 {
--- Eksamensprojekt/BushcraftBlog/Server/Models/BushcraftBlogDBContext.cs
+++ Eksamensprojekt/BushcraftBlog/Server/Models/BushcraftBlogDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using BushcraftBlog.Shared.Models;
 
 namespace BushcraftBlog.Server.Models;
 
