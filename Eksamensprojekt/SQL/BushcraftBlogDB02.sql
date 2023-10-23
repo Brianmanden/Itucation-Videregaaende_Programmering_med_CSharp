ae2c90d36520f2f2dcdc178e5c9296d8101e539c
Brian Andersen <brianmanden@gmail.com>
2023-10-23 14:49:25 +0200

    ADD SQL SELECT from all tables

---
+++ Eksamensprojekt/SQL/BushcraftBlogDB02.sql
@@ -0,0 +1,16 @@
+USE BushcraftBlogDB;
+
+SELECT TOP (1000) *
+FROM [BushcraftBlogDB].[dbo].[Documents]
+
+SELECT TOP (1000) *
+FROM [BushcraftBlogDB].[dbo].[Topic]
+
+SELECT TOP (1000) *
+FROM [BushcraftBlogDB].[dbo].[Tag]
+
+SELECT TOP (1000) *
+FROM [BushcraftBlogDB].[dbo].[DocumentTag]
+
+SELECT TOP (1000) *
+FROM [BushcraftBlogDB].[dbo].[DocumentTopic]
